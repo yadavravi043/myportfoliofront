5109029909adbf864e2539bb22099d4c24ce7b3a
yadavravi043 <ravi18016925@gmail.com>
2022-11-28 00:30:34 +0530

    fix

--- src/pages/project.js
+++ src/pages/project.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Footer from '../component/Footer/Footer'
-import HeroImg2 from '../component/HeroimgProj/HeroImg2'
+import HeroImg2 from '../component/HeroimgAll/HeroImg2'
 import Navbar from '../component/Navbar/Navbar'
 const Project = () => {
   return (
